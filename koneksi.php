11f68b39948bd7de56897b0e373a2e4a2ee643f2
tomyrizki <64814677+tomyrizki@users.noreply.github.com>
2020-05-13 09:44:38 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,17 @@
+<?php
+    $host = "localhost"; //host database
+    $username = "root"; //username database
+    $password = ""; //password database
+    $db = "profile";
+
+    $koneksi = new mysqli($host, $username, $password, $db); //membuat koneksi ke server mysql
+
+    if ($koneksi->connect_error) 
+    {
+        die("Koneksi ke database gagal");
+    }
+    else
+    {
+        echo "Koneksi berhasil <br>";
+    }
+?>
